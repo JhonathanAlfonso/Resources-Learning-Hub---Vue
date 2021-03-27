58a6c889b71978b99b9aa82473261c1c3ff4d343
Jhonathan Salazar <jhonathan.alfonso@evertecinc.com>
2021-03-27 15:43:28 -0500

    feat: project init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul></ul>        
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                { 
+                    id: 'official-guide', 
+                    title: 'Official Guide',
+                    description: 'The official Vue.js docs',
+                    link: 'https://vuejs.org'
+                 },
+                 { 
+                    id: 'google', 
+                    title: 'Google',
+                    description: 'Google source',
+                    link: 'https://google.com'
+                 }
+            ]
+        }
+    },
+}
+</script>
