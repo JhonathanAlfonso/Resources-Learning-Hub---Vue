198b5123fb2619fcadeb51c3b2e5f4fb6c0683a6
Jhonathan Salazar <jhonathan.alfonso@evertecinc.com>
2021-05-01 10:57:33 -0500

    style: basic button with styling

--- src/components/learning-resource/ResourceItem.vue
+++ src/components/learning-resource/ResourceItem.vue
@@ -3,7 +3,7 @@
         <base-card>
             <header>
                 <h3>{{ title }}</h3>
-                <button>Delete</button>
+                <base-button mode="flat">Delete</base-button>
             </header>
             <p>{{ description }}</p>
             <nav>
@@ -23,7 +23,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 li {
   margin: auto;
   max-width: 40rem;
